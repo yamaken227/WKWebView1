27135816eb01fa7e3c788433bcbe56385b615192
yamaken227 <74579887+yamaken227@users.noreply.github.com>
2021-03-25 11:26:04 +0900

    first commit

--- WKWebView1/ViewController.swift
+++ WKWebView1/ViewController.swift
@@ -6,14 +6,64 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,WKNavigationDelegate {
+    
+    @IBOutlet weak var indicator: UIActivityIndicatorView!
+    
+    var webView = WKWebView()
+        
+    @IBOutlet weak var toorBar: UIToolbar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        indicator.isHidden = true
+        
+        //大きさを決める
+        
+        webView.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height - toorBar.frame.size.height)
+        
+        view.addSubview(webView)
+        
+        webView.navigationDelegate = self
+        
+        //URLをロード
+        let url = URL(string: "https://www.kurashiru.com/")
+        let request = URLRequest(url:url!)
+        webView.load(request)
+        
+        indicator.layer.zPosition = 2
+        
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        
+        //ロードが完了した時に呼ばれるデリゲートメソッド
+        indicator.isHidden = true
+        indicator.stopAnimating()
+    }
+    
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+        
+        //読み込みが開始した時に呼ばれるデリゲートメソッド
+        indicator.isHidden = false
+        indicator.startAnimating()
     }
 
-
+    @IBAction func back(_ sender: Any) {
+        
+        webView.goBack()
+        
+    }
+    
+    @IBAction func go(_ sender: Any) {
+        
+        webView.goForward()
+        
+    }
+    
+    
 }
 
