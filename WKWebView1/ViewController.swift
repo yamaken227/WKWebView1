a1f18ba5a46e1829deccc756afc0e5b2a0190757
yamaken227 <74579887+yamaken227@users.noreply.github.com>
2021-03-25 10:38:48 +0900

    Initial Commit

---
+++ WKWebView1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WKWebView1
+//
+//  Created by Kenta Yamada on 2021/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
